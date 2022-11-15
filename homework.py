e9478a5bc0756ac53b6e1d4527b27f723de54e85
vail0 <97312541+vail0@users.noreply.github.com>
2022-11-16 02:51:57 +0300

    all functions but main are ready

--- homework.py
+++ homework.py
@@ -1,11 +1,23 @@
-...
+import logging
+import os
+import requests
+import telegram
+import time
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+token_list = (
+    PRACTICUM_TOKEN,
+    TELEGRAM_TOKEN,
+    TELEGRAM_CHAT_ID,    
+)
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,44 +30,79 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def send_message(bot, message):
-    ...
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.info(f'Message \'{message}\' sent')
+    except Exception as error:
+        logger.error(f'Бот не смог отправить сообщение: ошибка {error}')
 
+# print(send_message(bot = telegram.Bot(token=TELEGRAM_TOKEN), message = 'test_message'))
 
 def get_api_answer(current_timestamp):
+    '''Получение ответа от Яндекс Практикума'''
+    url = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    try:
+        homework_statuses = requests.get(url, headers=headers, params=params)
+        return homework_statuses.json()
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к API Яндекса {error}')
 
-    ...
+# print(get_api_answer(1549962000))
 
 
 def check_response(response):
+    '''Проверка корректности полученого json ответа'''
+    try:
+        resp = response.get('homeworks')[0]
+        return resp
+    except:
+        return 'Нет дз за данный период'
 
-    ...
-
+# print(check_response(get_api_answer(1549962000)))
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    '''Перевод статуса дз из json на человеческий язык'''
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
 
-    ...
+    if (homework_name is None) or (homework_status is None):
+        raise Exception(f'Имя или статус отсутсвуют {homework}')
 
-    verdict = ...
-
-    ...
+    verdict = HOMEWORK_STATUSES[homework_status]
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
+def namestr(obj, namespace):
+    return [name for name in namespace if namespace[name] is obj]
 
 def check_tokens():
-    ...
+    ''' Проверка наличия токенов пользователя'''
+    for token in token_list:
+        if len(token) == 0:
+            logger.error(f'Отсутствует {namestr(token, globals())[0]}')
+            return False
+        else:    
+            print(f'{namestr(token, globals())[0]} существует')
+    return True
 
+# check_tokens()
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    if check_response() is False:
+        pass
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
